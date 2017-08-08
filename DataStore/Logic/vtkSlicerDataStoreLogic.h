177894305f754d0b62a1267d8fd1e862862bf735
Hina Shah <hina.shah@kitware.com>
2017-08-08 11:21:38 -0400

    COMP adding VTK_OVERRIDE

--- DataStore/Logic/vtkSlicerDataStoreLogic.h
+++ DataStore/Logic/vtkSlicerDataStoreLogic.h
@@ -48,7 +48,7 @@ public:
 
   static vtkSlicerDataStoreLogic *New();
   vtkTypeMacro(vtkSlicerDataStoreLogic, vtkSlicerModuleLogic);
-  void PrintSelf(ostream& os, vtkIndent indent);
+  void PrintSelf(ostream& os, vtkIndent indent) VTK_OVERRIDE;
   
   // Load a MRML File.
   void LoadMRMLScene(QString mrmlFilePath);
@@ -60,13 +60,13 @@ protected:
   vtkSlicerDataStoreLogic();
   virtual ~vtkSlicerDataStoreLogic();
 
-  virtual void SetMRMLSceneInternal(vtkMRMLScene* newScene);
+  virtual void SetMRMLSceneInternal(vtkMRMLScene* newScene) VTK_OVERRIDE;
   
   /// Register MRML Node classes to Scene. Gets called automatically when the MRMLScene is attached to this logic class.
-  virtual void RegisterNodes();
-  virtual void UpdateFromMRMLScene();
-  virtual void OnMRMLSceneNodeAdded(vtkMRMLNode* node);
-  virtual void OnMRMLSceneNodeRemoved(vtkMRMLNode* node);
+  virtual void RegisterNodes() VTK_OVERRIDE;
+  virtual void UpdateFromMRMLScene() VTK_OVERRIDE;
+  virtual void OnMRMLSceneNodeAdded(vtkMRMLNode* node) VTK_OVERRIDE;
+  virtual void OnMRMLSceneNodeRemoved(vtkMRMLNode* node) VTK_OVERRIDE;
 private:
 
   vtkSlicerDataStoreLogic(const vtkSlicerDataStoreLogic&); // Not implemented
